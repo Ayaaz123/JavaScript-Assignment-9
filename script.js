2cf85cefcc4db48a1f25aed41ead2ffbc07e6c03
Ayaaz123 <ayaazsalim123@gmail.com>
2024-08-05 20:26:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const changelogo = () => {
+    const logo = document.getElementById('logo');
+    logo.src = 'https://www.hatchwise.com/wp-content/uploads/2022/05/amazon-logo-1536x1024.png.webp'; 
+};
+
+const reset = () => {
+    const logo = document.getElementById('logo');
+    logo.src = 'https://www.logoai.com/uploads/articles/2024/03/05/tesla-logo-white-1709619543.jpg'; 
+};
